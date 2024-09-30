88c0937c883d24b800f411648700996d68578a19
Dawson French <wdfrench2006@gmail.com>
2024-09-30 15:55:11 +0100

    Create lab-01-p2.main.kts

---
+++ lab-01-p2.main.kts
@@ -0,0 +1,15 @@
+// Returns the first letter and the length of a given word
+fun bingoWord(word: String): String {
+    return word.first().uppercase() + " " + word.length
+
+    // If adjusted for inputs where the input type could be a variable type other than a String
+    /* when (word) {
+        is String -> return word.first().uppercase() + " " + word.length
+        else -> return println("Invalid input. Please try again.")
+    } */
+}
+
+// Runs the program
+fun main() {
+    println(bingoWord("Birthday"))
+}
